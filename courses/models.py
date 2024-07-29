9c8a087433a81446b221639ac8c74b8eb0d65d6f
= <nozimabonuxamidova2@gmail.com>
2024-07-29 23:05:48 +0500

    commit

--- courses/models.py
+++ courses/models.py
@@ -1,34 +1,60 @@
 from django.db import models
-from teacher.models import Teacher
+from blogs.models import Author, Blog
+from teachers.models import Teacher
 
-
-# Create your models here.
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = "Categories"
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
-class Courses(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    num_students = models.IntegerField(default=0)
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    number_of_students = models.IntegerField(default=0)
     price = models.FloatField()
     duration = models.IntegerField()
-    video = models.FileField(upload_to='videos/')
-    teachers = models.ManyToManyField(Teacher)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    teachers = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    video = models.FileField(upload_to='media/courses')
+    category = models.ForeignKey(Category, related_name='courses', on_delete=models.CASCADE, null=True, blank=True)
 
     @property
-    def total_time(self):
-        return self.duration / 60
+    def hours(self):
+        if self.duration >= 60:
+            hours = self.duration // 60
+            return hours
+
+    @property
+    def minutes(self):
+        if self.duration >= 60:
+            minutes = self.duration % 60
+            return minutes
+
+    objects = models.Manager
 
     def __str__(self):
-        return self.name
+        return self.title
 
 
-class Comments(models.Model):
-    pass
+class Comment(models.Model):
+    class RatingChoices(models.TextChoices):
+        Zero = '0'
+        One = '1'
+        Two = '2'
+        Three = '3'
+        Four = '4'
+        Five = '5'
+
+    name = models.CharField(max_length=50)
+    email = models.EmailField(null=True, blank=True)
+    comment = models.TextField()
+    is_published = models.BooleanField(default=False)
+    rating = models.CharField(max_length=100, choices=RatingChoices.choices, default=RatingChoices.Zero.value)
+    course_id = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='comments')
+    blog_id = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='comments')
